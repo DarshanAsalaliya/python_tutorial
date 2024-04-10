2a288d7ba3c65ec56190d7d0e78fecb3920d58b1
Darshan Asalaliya <dkasalaliya@gmail.com>
2024-04-10 19:07:52 +0530

    if statement

---
+++ if/if_statement_one.py
@@ -0,0 +1,20 @@
+isMale = True
+isFat =True
+
+if isMale and isFat:
+    print("Male is Fat")
+else:
+    print("Female")
+
+
+isCricketer = True
+isFootballer = False
+
+if isCricketer and isFootballer:
+    print("All sport")
+elif isCricketer:
+    print("Cricketer")
+elif isFootballer:
+    print("Footballer")
+else:
+    print("No Sportman")
