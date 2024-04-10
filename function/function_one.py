d6d676f4926c620f2cb8d854d316802534933c2c
Darshan Asalaliya <dkasalaliya@gmail.com>
2024-04-10 18:47:01 +0530

    function

---
+++ function/function_one.py
@@ -0,0 +1,21 @@
+def hello():
+    print("Hellow world!")
+
+
+hello()
+
+
+def intro(name,age):
+    print("Helle, i am "+name+". i am "+age+" old")
+
+
+intro("Darshan","22")
+
+
+# return statement
+def cube(num):
+    return num*num*num
+
+
+resultOfCube = cube(5)
+print(resultOfCube)
