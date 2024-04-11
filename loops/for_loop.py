3ab019efa0fdfa0ac5c78fff1e2324d5bee150ad
Darshan Asalaliya <dkasalaliya@gmail.com>
2024-04-11 15:47:15 +0530

    for loop

---
+++ loops/for_loop.py
@@ -0,0 +1,21 @@
+
+friends = ["dev","darshan","vatsal"]
+
+
+print("this is frds name")
+for name in friends:
+    print(name)
+print("")
+print("")
+print("this is letter")
+for letter in "Darshan Asalaliya":
+    print(letter)
+print("")
+print("")
+print("This is range")
+for index in range(3, 10):
+    print(index)
+print("")
+print("")
+print("The length of frd")
+print(len(friends))
